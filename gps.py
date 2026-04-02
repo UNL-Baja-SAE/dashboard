cfcc1c87b5a359e03b955973da62e26451df30be
Zach Anderson <anderson.zachary.m@gmail.com>
2026-04-02 18:52:54 -0500

    Update gps back to 1Hz

--- gps.py
+++ gps.py
@@ -8,15 +8,6 @@ BAUD = 9600
 # Initialize globally, but set to None if it fails
 try:
     ser = serial.Serial(PORT, baudrate=BAUD, timeout=0.5)
-
-    ser.write(b'$PUBX,40,GGA,0,0,0,0,0,0*5A\r\n')
-    ser.write(b'$PUBX,40,GSA,0,0,0,0,0,0*4E\r\n')
-    ser.write(b'$PUBX,40,GSV,0,0,0,0,0,0*59\r\n')
-    ser.write(b'$PUBX,40,GLL,0,0,0,0,0,0*5C\r\n')
-    ser.write(b'$PUBX,40,VTG,0,0,0,0,0,0*5E\r\n')
-    
-    # 2. Send the binary UBX command to set the refresh rate to 5Hz (200ms)
-    ser.write(b'\xB5\x62\x06\x08\x06\x00\xC8\x00\x01\x00\x01\x00\xDE\x6A')
     
     # Give the module half a second to apply the new settings
     time.sleep(0.5)
